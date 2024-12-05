0ad158d18633103e4806332f03ab34c5d494dec0
league <league@wts14-3.attlocal.net>
2024-12-04 17:00:57 -0800

    AM ON find hidden button on intro to arrays

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -5,32 +5,58 @@ import java.util.Random;
 public class _01_IntroToArrays {
     public static void main(String[] args) {
         // 1. declare and Initialize an array 5 Strings
-
+    	String[] strings = new String[5];
+    	strings[0] = "A";
+    	strings[1] = "B";
+    	strings[2] = "C";
+    	strings[3] = "D";
+    	strings[4] = "E";
         // 2. print the third element in the array
-
+    	System.out.println(strings[2]);
         // 3. set the third element to a different value
-
+    	strings[2] = "Dog";
         // 4. print the third element again
-
+    	System.out.println(strings[2]);
         // 5. use a for loop to set all the elements in the array to a string
         //    of your choice
-
+    	for(int i=0; i<strings.length; i++) {
+    		strings[i] = "Coco";
+    	}
         // 6. use a for loop to print all the values in the array
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
-
+    	for(int i=0; i<strings.length; i++) {
+    	String s = strings[i];
+    	System.out.println("String at element " + i + " " + s);
+    	}
+    	System.out.println("\n");
         // 7. make an array of 50 integers
-
+    	int[] ints = new int[50];
         // 8. use a for loop to make every value of the integer array a random
         //    number
-
+    	Random ran = new Random();
+    	for(int i =0; i<ints.length; i++) {
+    		int a = ran.nextInt(50);
+    		ints[i] = a;
+    	}
         // 9. without printing the entire array, print only the smallest number
         //    on the array
-
-        // 10 print the entire array to see if step 8 was correct
+    	int smallest = ints[0];
+    	int largest = ints[0];
+    	for(int i = 0; i<ints.length; i++) {
+    	
+    		if(ints[i]<smallest) {
+    			smallest = ints[i];
+    		}
+    		if(ints[i]>largest) {
+    			largest = ints[i];
+    		}
+    	}
+    	System.out.println(smallest);
+    	// 10 print the entire array to see if step 8 was correct
 
         // 11. print the largest number in the array.
-
+System.out.println(largest);
         // 12. print only the last element in the array
-
+System.out.println(ints[49]);
     }
 }
--- src/_00_Intro_To_Arrays/_02_RobotRace.java
+++ src/_00_Intro_To_Arrays/_02_RobotRace.java
@@ -7,18 +7,40 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class _02_RobotRace {
-    // 1. make a main method
+	// 1. make a main method
+public static void main(String[] args) {
+	
 
         // 2. create an array of 5 robots.
+	Robot[] robots = new Robot[5];
 
         // 3. use a for loop to initialize the robots.
-
+	
+	for(int i = 0; i<robots.length; i++) {
+		robots[i] = new Robot("rob");
+		robots[i].setSpeed(100);
+		robots[i].setY(500);
+		robots[i].setX(50);
+	}
         // 4. make each robot start at the bottom of the screen, side by side, facing up
-    
+    	
+	Robot.setWindowSize(1920, 1000);
         // 5. use another for loop to iterate through the array and make each robot move
         // a random amount less than 50.
     
-        // 6. use a while loop to repeat step 5 until a robot has reached the top of the
+		//boolean crossed = false;
+		//while (crossed == false) {
+		//	Random ran = new Random();
+		//	for (int i = 0; i < robots.length; i++) {
+		//		int a = ran.nextInt(50);
+		//		robots[i].move(a);
+		//		if (robots[i].getY() < 0) {
+		//			crossed = true;
+		//			JOptionPane.showMessageDialog(null, "Robot " + (i + 1) + " won!");
+		//			break;
+		//		}
+		//	}
+		//} // 6. use a while loop to repeat step 5 until a robot has reached the top of the
         // screen.
     
         // 7. declare that robot the winner and throw it a party!
@@ -26,5 +48,22 @@ public class _02_RobotRace {
         // 8. try different races with different amounts of robots.
     
         // 9. make the robots race around a circular track.
-
+		boolean crossing = false;
+		while (crossing == false) {
+			Random ran = new Random();
+			for (int i = 0; i < robots.length; i++) {
+				if(ran.nextBoolean() == true) {
+				
+				robots[i].move(40);
+				robots[i].turn(5);
+				if (robots[i].getY() == 500 && robots[i].getX() == 50) {
+					crossing = true;
+					JOptionPane.showMessageDialog(null, "Robot " + (i + 1) + " won!");
+					break;
+				}
+				}
+				
+			}
+		}
+}
 }
